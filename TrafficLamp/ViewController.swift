70ec3c778e3df5498d8bdbd1f353cb3e8bff0020
meirsusan <59894052+meirsusan@users.noreply.github.com>
2020-09-08 15:23:11 +0300

    Initial Commit

---
+++ TrafficLamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLamp
+//
+//  Created by Meir Susan on 8.09.2020.
+//  Copyright © 2020 Meir Susan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
